4f8ad350814beb230a7d3fa7f3593bfe37aec9f3
Songwj <145970884+songwj123@users.noreply.github.com>
2024-05-08 15:19:02 +0800

    上传模块路由

--- app/goods/urls.py
+++ app/goods/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('get_product/<str:product_id>', views.get_product, name='get_product'),
     path('update_product/<str:product_id>', views.update_product, name='update_product'),
     path('delete_product/<str:product_id>', views.delete_product, name='delete_product'),
+    path('upload_csv', views.upload_csv, name='upload_csv'),
 ]
