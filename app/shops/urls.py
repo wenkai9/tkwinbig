d207f8e7eb3ca0d8664aa4905e3629c9f0d93aaf
Songwj <145970884+songwj123@users.noreply.github.com>
2024-05-09 17:14:29 +0800

    shops路由

--- app/shops/urls.py
+++ app/shops/urls.py
@@ -3,9 +3,14 @@ from . import views
 
 urlpatterns = [
     # path('home/', views.home, name='home'),
+    # 创建商铺
     path('create_shop/', views.create_shop, name='create_shop'),
+    # 商铺列表
     path('list_shops/', views.list_shops, name='list_shops'),
+    # 商铺详情
     path('view_shop/<int:shopId>/', views.view_shop, name='view_shop'),
+    # 商铺更新
     path('update_shop/<int:shopId>/', views.update_shop, name='update_shop'),
+    # 商铺删除
     path('delete_shop/<int:shopId>/', views.delete_shop, name='delete_shop'),
 ]
