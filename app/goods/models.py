e21347d8e6d275a26fe5355bc9c26d32a0d756df
Songwj <145970884+songwj123@users.noreply.github.com>
2024-05-21 11:39:40 +0800

    模型迁移

--- app/goods/models.py
+++ app/goods/models.py
@@ -2,19 +2,43 @@ from django.db import models
 from app.shops.models import Shop
 
 
+class base_category1(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'base_category1'
+
+
+class base_category2(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    category1 = models.ForeignKey(base_category1, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'base_category2'
+
+
 class Goods(models.Model):
-    # product_id = models.CharField(max_length=100, primary_key=True)
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    category_id = models.CharField(max_length=50)
-    primary_category = models.CharField(max_length=50)
-    secondary_category = models.CharField(max_length=50)
+    hasFreeSample = models.BooleanField(default=False)
+    commissionRate = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    CooperationFee = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    base_category2 = models.ForeignKey(base_category2, on_delete=models.CASCADE)
     product_link = models.TextField()
-    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)  # 外键关联到 Shop 模型
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     match_tag = models.CharField(max_length=100)
-    createdAt = models.DateTimeField(auto_now_add=True)  # auto_now_add 用于记录创建时间
-    updatedAt = models.DateTimeField(auto_now=True)  # auto_now 用于记录更新时间
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
--- app/task/models.py
+++ app/task/models.py
@@ -10,16 +10,16 @@ class Task(models.Model):
         ('2', '正在进行'),
         ('3', '已完成'),
     )
-
-    taskId = models.IntegerField(primary_key=True)
+    taskId = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     product = models.ForeignKey(Goods, on_delete=models.CASCADE)
     region = models.CharField(max_length=20)
     status = models.CharField(max_length=1, choices=TASK_STATUS_CHOICES,default='1')
-    match_quantity = models.IntegerField()
-    willing_quantity = models.IntegerField()
-    send_quantity = models.IntegerField()
+    match_quantity = models.IntegerField(null=True)
+    willing_quantity = models.IntegerField(null=True)
+    send_quantity = models.IntegerField(null=True)
     createAt = models.DateTimeField(auto_now_add=True)
     updateAt = models.DateTimeField(auto_now=True)
 
