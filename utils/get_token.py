89dcb3963dfa61711a13f084cfe09ea517ece6b0
Songwj <145970884+songwj123@users.noreply.github.com>
2024-09-02 19:27:52 +0800

    获取rpa的token修正

--- utils/get_token.py
+++ utils/get_token.py
@@ -1,26 +1,20 @@
 import base64
 import requests
 
-'''
-target_commission
-open_commission
-effective_status
-'''
 
-
-def get_token():
+def get_token(username, password):
     try:
-        your_string = "song:306012"
+        your_string = f"{username}:{password}"
         b = base64.b64encode(your_string.encode('utf-8'))
         url = "https://qtoss-connect.azurewebsites.net/token"
         base64_str = b.decode('utf-8')
         authorization = 'Basic ' + base64_str
+        print("authorization", authorization)
         response = requests.post(url=url, headers={'Content-Type': 'application/json',
                                                    'Authorization': authorization})
         data = response.json()
         access_token = data['access_token']
         if response.status_code == 200:
-            # print(access_token)
             return access_token
         else:
             return None
