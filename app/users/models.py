160cd9a6f96ffc21f531088a9a880c5c10012985
Songwj <145970884+songwj123@users.noreply.github.com>
2024-09-14 09:47:12 +0800

    新增用户上传头像接口(模型)

--- app/users/models.py
+++ app/users/models.py
@@ -7,6 +7,7 @@ class User(models.Model):
     password = models.CharField(max_length=128)
     email = models.EmailField()
     number = models.CharField(max_length=20)
+    avatar_url = models.TextField(null=True, blank=True)
     company = models.CharField(max_length=100)
     create_at = models.DateTimeField(auto_now_add=True)
 
