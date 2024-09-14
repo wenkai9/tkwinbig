0b8e021b2f178ddb847862747e63b3709a8c61b1
Songwj <145970884+songwj123@users.noreply.github.com>
2024-09-14 09:47:03 +0800

    新增用户上传头像接口(路由)

--- app/users/urls.py
+++ app/users/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('user/register', views.user_register, name='register'),
     path('user/login', views.user_login, name='user_login'),
     path('user/user_profile', views.user_profile, name='user_profile'),
+    path('user/upload_photos', views.upload_photos, name='upload_photos'),
     path('user/change_password', views.change_password, name='change_password'),
     path('user/logout', views.user_logout, name='user_logout'),
 ]
